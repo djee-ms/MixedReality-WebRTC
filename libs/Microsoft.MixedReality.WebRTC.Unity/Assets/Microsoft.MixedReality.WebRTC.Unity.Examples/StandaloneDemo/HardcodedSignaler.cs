7a2fbeb4b2c1493a450a64c752bd220df2f81969
Jerome Humbert <jerome.humbert@microsoft.com>
2020-01-22 15:19:53 +0000

    [Unity] Add hard-coded signaler for demos
    Add a new HardcodedSignaler component acting as a signaling solution
    between two PeerConnection components present in the same Unity scene.
    
    This is mainly used for demos, to avoid the need to have a node-dss
    server running or any other kind of networking, and make the demo
    self-contained, similar to the official JavaScript WebRTC demos.

---
+++ libs/Microsoft.MixedReality.WebRTC.Unity/Assets/Microsoft.MixedReality.WebRTC.Unity.Examples/StandaloneDemo/HardcodedSignaler.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using UnityEngine;
+using UnityEngine.UI;
+using Microsoft.MixedReality.WebRTC.Unity;
+
+/// <summary>
+/// Simple signaler using two peer connections in the same process,
+/// and hard-coding their SDP message delivery to avoid the need for
+/// any kind of networking to deliver SDP messages.
+/// 
+/// This component is designed to be used in demos where both peers
+/// are present in the same scene.
+/// </summary>
+public class HardcodedSignaler : MonoBehaviour
+{
+    /// <summary>
+    /// First peer to connect, which will generate an offer.
+    /// </summary>
+    public PeerConnection Peer1;
+
+    /// <summary>
+    /// Second peer to connect, which will wait for an offer from the first peer.
+    /// </summary>
+    public PeerConnection Peer2;
+
+    /// <summary>
+    /// The button that generates an offer from peer #1 to peer #2.
+    /// </summary>
+    [Tooltip("The button that generates an offer to a given target")]
+    public Button CreateOfferButton;
+
+    private void Start()
+    {
+        Peer1.OnInitialized.AddListener(OnInitialized1);
+        Peer2.OnInitialized.AddListener(OnInitialized2);
+
+        CreateOfferButton.onClick.AddListener(() =>
+        {
+            Peer1.CreateOffer();
+        });
+    }
+
+    private void OnInitialized1()
+    {
+        Peer1.Peer.LocalSdpReadytoSend += Peer1_LocalSdpReadytoSend;
+        Peer1.Peer.IceCandidateReadytoSend += Peer1_IceCandidateReadytoSend;
+    }
+
+    private void OnInitialized2()
+    {
+        Peer2.Peer.LocalSdpReadytoSend += Peer2_LocalSdpReadytoSend;
+        Peer2.Peer.IceCandidateReadytoSend += Peer2_IceCandidateReadytoSend;
+    }
+
+    private async void Peer1_LocalSdpReadytoSend(string type, string sdp)
+    {
+        await Peer2.Peer.SetRemoteDescriptionAsync(type, sdp);
+        if (type == "offer")
+        {
+            Peer2.CreateAnswer();
+        }
+    }
+
+    private async void Peer2_LocalSdpReadytoSend(string type, string sdp)
+    {
+        await Peer1.Peer.SetRemoteDescriptionAsync(type, sdp);
+        if (type == "offer")
+        {
+            Peer1.CreateAnswer();
+        }
+    }
+
+    private void Peer1_IceCandidateReadytoSend(string candidate, int sdpMlineindex, string sdpMid)
+    {
+        Peer2.Peer.AddIceCandidate(sdpMid, sdpMlineindex, candidate);
+    }
+
+    private void Peer2_IceCandidateReadytoSend(string candidate, int sdpMlineindex, string sdpMid)
+    {
+        Peer1.Peer.AddIceCandidate(sdpMid, sdpMlineindex, candidate);
+    }
+}
