3bc54534a81491ef85e51b1f804c1ac58a8cc9db
Jerome Humbert <jerome.humbert@microsoft.com>
2020-04-02 09:30:56 +0100

    Add glob <mrwebrtc/mrwebrtc.h> header

---
+++ libs/mrwebrtc/include/mrwebrtc/mrwebrtc.h
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include <mrwebrtc/audio_frame.h>
+#include <mrwebrtc/data_channel_interop.h>
+#include <mrwebrtc/export.h>
+#include <mrwebrtc/external_video_track_source_interop.h>
+#include <mrwebrtc/interop_api.h>
+#include <mrwebrtc/local_audio_track_interop.h>
+#include <mrwebrtc/local_video_track_interop.h>
+#include <mrwebrtc/peer_connection_interop.h>
+#include <mrwebrtc/remote_audio_track_interop.h>
+#include <mrwebrtc/remote_video_track_interop.h>
+#include <mrwebrtc/transceiver_interop.h>
+#include <mrwebrtc/video_frame.h>
